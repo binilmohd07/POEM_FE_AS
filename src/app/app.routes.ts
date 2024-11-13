bdddd1d66b46b79916300d373c9eb0163a93b0e0
binilmohamed07 <binilmohamed07@gmail.com>
2024-11-13 22:32:24 +0530

    Basic routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/commons/login/login.component';
+import { ErrorsComponent } from './components/commons/errors/errors.component';
+import { BaseComponent } from './components/commons/base/base.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { FinancesComponent } from './components/finances/finances.component';
+import { OperationsComponent } from './components/operations/operations.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'login', component: LoginComponent
+    }, {
+        path : 'home', component: BaseComponent,
+        children : [
+            {
+                path : 'finances', component: FinancesComponent
+            }, {
+                path : 'operations', component: OperationsComponent
+            }, {
+                path : 'settings', component: SettingsComponent
+            }
+        ]
+    }, {
+        path:'', redirectTo:'/home', pathMatch:'full'
+    }, {
+        path : '**', pathMatch:'full', component:ErrorsComponent
+    }
+];
