043c8f06bb332d864c1cd6e91e1cec42c32bdba5
binilmohamed07 <binilmohamed07@gmail.com>
2024-11-15 13:22:25 +0530

    Angular material setup completed

--- src/app/components/commons/base/base.component.ts
+++ src/app/components/commons/base/base.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-base',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './base.component.html',
   styleUrl: './base.component.scss'
 })
